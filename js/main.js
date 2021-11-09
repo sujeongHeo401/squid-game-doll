a89e4210b5abfebc64d31538bbc0d19b0a673fb9
sujeongHeo401 <gotnwjd40@gmail.com>
2021-11-10 02:12:37 +0900

    카메라 설정

--- js/main.js
+++ js/main.js
@@ -10,4 +10,6 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 7;
+
+renderer.render(scene, camera);
